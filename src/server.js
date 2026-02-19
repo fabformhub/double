71d01302387a7a5c02e6e9504725454c2c628d69
Geoffrey Callaghan <irishgeoff@yaahoo.com>
2026-02-19 15:04:23 +0000

    changed port to 8081

--- src/server.js
+++ src/server.js
@@ -153,11 +153,11 @@ import messagesRoutes from "./routes/messages.js";    // /messages
 import apiRoutes from "./routes/api.js";              // /api/*
 import adsBrowseRoutes from "./routes/adsBrowse.js";
 import pageRoutes from "./routes/pages.js";
-import paymentRoutes from "./routes/paymentRoutes.js";
+// import paymentRoutes from "./routes/paymentRoutes.js";
 
 // SYSTEM ROUTES
 
-app.use("/", paymentRoutes);
+// app.use("/", paymentRoutes);
 app.use("/", authRoutes);
 app.use("/", pageRoutes);
 app.use("/dashboard", dashboardRoutes);
